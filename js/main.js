4f80f96c137630eb02170a8b329565ac2dec33dd
Dubsta <dubsta.post@yandex.ru>
2019-10-13 21:56:06 +0300

    Almost all project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      auto: true,
+      controls: true,
+      pager: false,
+      nextText: '<i class="fa fa-arrow-right"></i>',
+      prevText: '<i class="fa fa-arrow-left"></i>'
+    });
+  });
