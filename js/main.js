ac6d6151dba93db24a1fe8910088010df4a102fc
Dubsta <dubsta.post@yandex.ru>
2019-10-14 22:49:55 +0300

    New block + CSS, animation for slider hover

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-$(document).ready(function(){
-    $('.slider').bxSlider({
-      auto: true,
-      controls: true,
-      pager: false,
-      nextText: '<i class="fa fa-arrow-right"></i>',
-      prevText: '<i class="fa fa-arrow-left"></i>'
-    });
+$(document).ready(function () {
+  $('.slider').bxSlider({
+    auto: false,
+    controls: true,
+    pager: false,
+    nextText: '<i class="fa fa-arrow-right"></i>',
+    prevText: '<i class="fa fa-arrow-left"></i>'
   });
+});
+
+//Showing logo descripting on hover
+let partnerBlock = document.querySelectorAll('.partner-list a');
+partnerBlock.forEach((item) => {
+  item.addEventListener('mouseover', (event) => {
+    let img = item.querySelector('img');
+    let p = item.querySelector('p');  
+
+    p.style.opacity = '1';
+    img.style.display = 'none';    
+  });
+
+  item.addEventListener('mouseleave', (event) => {
+    let img = item.querySelector('img');
+    let p = item.querySelector('p');
+   
+    p.style.opacity = '0';
+    img.style.display = 'block';   
+  });
+});
